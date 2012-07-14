4b8660078423b19bf39cda95039ef49002860554
Leif Theden <leif.theden@gmail.com>
2012-07-14 12:57:39 -0500

    added misc files

---
+++ lib2d/animation.py
@@ -0,0 +1,202 @@
+from objects import GameObject
+import res
+
+from collections import namedtuple
+import math, itertools
+
+
+pi2 = math.pi * 2
+
+box = namedtuple('Box', 'width height')
+frame = namedtuple('Frame', 'box image')
+
+
+"""
+animations may be used by a class that does not need the images, but may need
+to know when an animation finishes, where it is at, etc (such as a server).
+"""
+
+def calcFrameSize(filename, frames, directions):
+    """
+    load the images for the animation and set the size of each frame
+    """
+
+    width, height = res.loadImage(filename, 0,0,1).get_size()
+    return (width / frames, height / directions)
+
+
+class Animation(GameObject):
+    """
+    Animation is a collection of frames with a few control variables and useful
+    methods to control it.
+
+    Animations can store multiple directions and are picklable.
+
+    each set of animation added will count as a seperate direction.
+    1 animation  = no rotations
+    2 animations = left and right (or up and down)
+    4 animations = left, right, up, down
+    6 animations = up, down, nw, ne, sw, se (for hex maps)
+    and so on.
+
+    The animation loader expects the image to be in a specific format.
+    Loader only supports animations where each frame is the same size.
+
+    TODO: implement some sort of timing, rather than relying on frames
+    """
+
+    def __init__(self, filename, name, frames, directions=1, timing=100):
+
+        GameObject.__init__(self)
+
+        self.filename = filename
+        self.name = name
+        self.images = None
+        self.directions = directions
+
+        # TODO: some checking to make sure inputs are the correct length
+        if isinstance(frames, int):
+            self.frames = tuple(range(0, frames))
+        else:
+            self.frames = tuple(frames)
+
+        self.real_frames = len(set(self.frames))
+
+        if isinstance(timing, int):
+            self.timing = tuple([timing] * len(self.frames))
+        else:
+            self.timing = tuple(timing)
+
+        self.iterator = iter(self)
+
+
+    def __iter__(self):
+        return itertools.cycle(zip(self.timing, self.frames))
+
+
+    def returnNew(self):
+        return self
+
+
+    def load(self, force=False):
+        """
+        load the images for use with pygame
+        returns a new Animation Object
+        """
+
+        if not self.images == None and not force:
+            return
+
+        image = res.loadImage(self.filename, 0, 0, 1)
+
+        iw, ih = image.get_size()
+        tw = iw / self.real_frames
+        th = ih / self.directions
+        self.images = [None] * (self.directions * self.real_frames)
+      
+        d = 0
+        for y in range(0, ih, th):
+            #if d == ih/th: d = 0
+            for x in range(0, iw, tw):
+                try:
+                    frame = image.subsurface((x, y, tw, th))
+                except ValueError as e:
+                    raise ValueError, self.filename
+                self.images[(x/tw)+d*self.real_frames] = frame
+            d += 1
+
+
+    def unload(self):
+        self.images = []
+
+
+    def advance(self):
+        return self.iterator.next()
+
+
+    def getImage(self, number,  direction=0):
+        """
+        return the frame by number with the correct image for the direction
+        direction should be expressed in radians
+        """
+
+        if self.images == []:
+            raise Exception, "Avatar hasn't loaded images yet"
+
+        if direction < 0:
+            direction = pi + (pi - abs(direction))
+        d = int(math.ceil(direction / pi2 * (self.directions - 1)))
+
+        try:
+            return self.images[number+d*self.real_frames]
+        except IndexError:
+            msg="{} cannot find image for animation ({}/{})"
+            raise IndexError, msg.format(self, number+d*self.real_frames,
+                                         len(self.images))
+
+
+    def __repr__(self):
+        return "<Animation %s: \"%s\">" % (id(self), self.filename)
+
+
+class StaticAnimation(Animation):
+    """
+    Animation that only supports one frame
+    Immutable
+    """
+
+    def __init__(self, filename, name, tile=None, size=None):
+        GameObject.__init__(self)
+
+        self.filename = filename
+        self.name = name
+        self.tile = tile
+        self.size = size
+        self.image = None
+
+
+    def loaded(self):
+        """
+        load the images for use with pygame
+        """
+
+        image = res.loadImage(self.filename, 0, 0, 1)
+      
+        if self.tile:
+            x, y = self.tile
+            x *= self.size[0]
+            y *= self.size[1]
+            ck = image.get_colorkey()
+            self.image = pygame.Surface(self.size)
+            self.image.blit(image,(0,0),area=(x,y, self.size[0], self.size[1]))
+            image.set_colorkey(ck, pygame.RELACCEL) 
+
+        else:
+            self.image = image
+
+        self.frames = [self.image]
+
+
+    def returnNew(self):
+        return self
+
+
+    def unload(self):
+        self.image = None
+
+
+    def getTTL(self, number):
+        return -1
+
+
+    def getImage(self, number, direction=0):
+        """
+        return the frame by number with the correct image for the direction
+        direction should be expressed in radians
+        """
+
+        return self.image
+
+    def __repr__(self):
+        return "<Animation %s: \"%s\">" % (id(self), self.filename)
+
---
+++ lib2d/vec2.py
@@ -0,0 +1,262 @@
+################## http://www.pygame.org/wiki/2DVectorClass ##################
+import operator
+import math
+ 
+class Vec2d(object):
+    """
+    Immutable 2d vector class, supports vector and scalar operators,
+    and also provides provides other high level functions.
+
+    Based on module from: http://www.pygame.org/wiki/2DVectorClass
+    Modified 2012, leif.theden@gmail.com
+    """
+
+    __slots__ = ['_x', '_y']
+ 
+    def __init__(self, x_or_pair, y = None):
+        if y == None:
+            self._x, self._y  = x_or_pair
+        else:
+            self._x = x_or_pair
+            self._y = y
+ 
+    def __len__(self):
+        return 2
+ 
+    def __getitem__(self, key):
+        if key == 0:
+            return self._x
+        elif key == 1:
+            return self._y
+        else:
+            raise IndexError("Invalid subscript "+str(key)+" to Vec2d")
+ 
+    # String representaion (for debugging)
+    def __repr__(self):
+        return 'Vec2d(%s, %s)' % (self._x, self._y)
+ 
+    # Comparison
+    def __eq__(self, other):
+        if hasattr(other, "__getitem__") and len(other) == 2:
+            return self._x == other[0] and self._y == other[1]
+        else:
+            return False
+ 
+    def __ne__(self, other):
+        if hasattr(other, "__getitem__") and len(other) == 2:
+            return self._x != other[0] or self._y != other[1]
+        else:
+            return True
+ 
+    def __nonzero__(self):
+        return bool(self._x or self._y)
+ 
+    # Generic operator handlers
+    def _o2(self, other, f):
+        "Any two-operator operation where the left operand is a Vec2d"
+        if isinstance(other, Vec2d):
+            return Vec2d(f(self._x, other.x),
+                         f(self._y, other.y))
+        elif (hasattr(other, "__getitem__")):
+            return Vec2d(f(self._x, other[0]),
+                         f(self._y, other[1]))
+        else:
+            return Vec2d(f(self._x, other),
+                         f(self._y, other))
+ 
+    def _r_o2(self, other, f):
+        "Any two-operator operation where the right operand is a Vec2d"
+        if (hasattr(other, "__getitem__")):
+            return Vec2d(f(other[0], self._x),
+                         f(other[1], self._y))
+        else:
+            return Vec2d(f(other, self._x),
+                         f(other, self._y))
+ 
+    def _io(self, other, f):
+        "inplace operator"
+        if (hasattr(other, "__getitem__")):
+            self._x = f(self._x, other[0])
+            self._y = f(self._y, other[1])
+        else:
+            self._x = f(self._x, other)
+            self._y = f(self._y, other)
+        return self
+ 
+    # Addition
+    def __add__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self._x + other.x, self._y + other.y)
+        elif hasattr(other, "__getitem__"):
+            return Vec2d(self._x + other[0], self._y + other[1])
+        else:
+            return Vec2d(self._x + other, self._y + other)
+    __radd__ = __add__
+ 
+    # Subtraction
+    def __sub__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self._x - other.x, self._y - other.y)
+        elif (hasattr(other, "__getitem__")):
+            return Vec2d(self._x - other[0], self._y - other[1])
+        else:
+            return Vec2d(self._x - other, self._y - other)
+    def __rsub__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(other.x - self._x, other.y - self._y)
+        if (hasattr(other, "__getitem__")):
+            return Vec2d(other[0] - self._x, other[1] - self._y)
+        else:
+            return Vec2d(other - self._x, other - self._y)
+ 
+    # Multiplication
+    def __mul__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self._x*other.x, self._y*other.y)
+        if (hasattr(other, "__getitem__")):
+            return Vec2d(self._x*other[0], self._y*other[1])
+        else:
+            return Vec2d(self._x*other, self._y*other)
+    __rmul__ = __mul__
+ 
+ 
+    # Division
+    def __div__(self, other):
+        return self._o2(other, operator.div)
+    def __rdiv__(self, other):
+        return self._r_o2(other, operator.div)
+ 
+    def __floordiv__(self, other):
+        return self._o2(other, operator.floordiv)
+    def __rfloordiv__(self, other):
+        return self._r_o2(other, operator.floordiv)
+ 
+    def __truediv__(self, other):
+        return self._o2(other, operator.truediv)
+    def __rtruediv__(self, other):
+        return self._r_o2(other, operator.truediv)
+ 
+    # Modulo
+    def __mod__(self, other):
+        return self._o2(other, operator.mod)
+    def __rmod__(self, other):
+        return self._r_o2(other, operator.mod)
+ 
+    def __divmod__(self, other):
+        return self._o2(other, operator.divmod)
+    def __rdivmod__(self, other):
+        return self._r_o2(other, operator.divmod)
+ 
+    # Exponentation
+    def __pow__(self, other):
+        return self._o2(other, operator.pow)
+    def __rpow__(self, other):
+        return self._r_o2(other, operator.pow)
+ 
+    # Bitwise operators
+    def __lshift__(self, other):
+        return self._o2(other, operator.lshift)
+    def __rlshift__(self, other):
+        return self._r_o2(other, operator.lshift)
+ 
+    def __rshift__(self, other):
+        return self._o2(other, operator.rshift)
+    def __rrshift__(self, other):
+        return self._r_o2(other, operator.rshift)
+ 
+    def __and__(self, other):
+        return self._o2(other, operator.and_)
+    __rand__ = __and__
+ 
+    def __or__(self, other):
+        return self._o2(other, operator.or_)
+    __ror__ = __or__
+ 
+    def __xor__(self, other):
+        return self._o2(other, operator.xor)
+    __rxor__ = __xor__
+ 
+    # Unary operations
+    def __neg__(self):
+        return Vec2d(operator.neg(self._x), operator.neg(self._y))
+ 
+    def __pos__(self):
+        return Vec2d(operator.pos(self._x), operator.pos(self._y))
+ 
+    def __abs__(self):
+        return Vec2d(abs(self._x), abs(self._y))
+ 
+    def __invert__(self):
+        return Vec2d(-self._x, -self._y)
+ 
+    # vectory functions
+    def get_length_sqrd(self):
+        return self._x**2 + self._y**2
+ 
+    def get_length(self):
+        return math.sqrt(self._x**2 + self._y**2)
+ 
+    def rotated(self, angle_degrees):
+        radians = math.radians(angle_degrees)
+        cos = math.cos(radians)
+        sin = math.sin(radians)
+        x = self._x*cos - self._y*sin
+        y = self._x*sin + self._y*cos
+        return Vec2d(x, y)
+ 
+    def get_angle(self):
+        if (self.get_length_sqrd() == 0):
+            return 0
+        return math.atan2(self._y, self._x)
+
+    def get_angle_between(self, other):
+        cross = self._x*other[1] - self._y*other[0]
+        dot = self._x*other[0] + self._y*other[1]
+        return math.degrees(math.atan2(cross, dot))
+ 
+    def normalized(self):
+        length = self.length
+        if length != 0:
+            return self/length
+        return Vec2d(self)
+ 
+    def normalize_return_length(self):
+        length = self.length
+        if length != 0:
+            self._x /= length
+            self._y /= length
+        return length
+ 
+    def perpendicular(self):
+        return Vec2d(-self._y, self._x)
+ 
+    def perpendicular_normal(self):
+        length = self.length
+        if length != 0:
+            return Vec2d(-self._y/length, self._x/length)
+        return Vec2d(self)
+ 
+    def dot(self, other):
+        return float(self._x*other[0] + self._y*other[1])
+ 
+    def get_distance(self, other):
+        return math.sqrt((self._x - other[0])**2 + (self._y - other[1])**2)
+ 
+    def get_dist_sqrd(self, other):
+        return (self._x - other[0])**2 + (self._y - other[1])**2
+ 
+    def projection(self, other):
+        other_length_sqrd = other[0]*other[0] + other[1]*other[1]
+        projected_length_times_other_length = self.dot(other)
+        return other*(projected_length_times_other_length/other_length_sqrd)
+ 
+    def cross(self, other):
+        return self._x*other[1] - self._y*other[0]
+ 
+    def interpolate_to(self, other, range):
+        return Vec2d(self._x + (other[0] - self._x)*range, self._y + (other[1] - self._y)*range)
+ 
+    def convert_to_basis(self, x_vector, y_vector):
+        return Vec2d(self.dot(x_vector)/x_vector.get_length_sqrd(), self.dot(y_vector)/y_vector.get_length_sqrd())
+ 
+
